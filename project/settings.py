7d5a4a1cc5abf6c09f543c10ffde49dbf89f2558
King-Keith1 <keith4321ben@gmail.com>
2025-04-05 14:02:49 +0200

    commit

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# Where your static files are located (e.g., dice/static/)
+STATICFILES_DIRS = [
+    BASE_DIR / "dice/static",
+]
+
+# Where collected static files will be stored after running collectstatic
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Debug setting (for development; set to False in production)
+DEBUG = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
